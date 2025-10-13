cbd94dd56087ca96d3ccefa59b79a17903db0ed4
Aleksandr <aleksandzarinskij18772@gmail.com>
2025-10-13 23:36:46 +0300

    iter4 - добавил инициализацию генератора без повторного вызова обработки.

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,15 +1,21 @@
 package app
 
-import "math/rand/v2"
+import (
+	"math/rand/v2"
+	"sync"
+)
 
 var (
 	chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	rng   *rand.Rand
+	once  sync.Once
 )
 
 // initRand - инициализирует генератора случайных чисел.
 func initRand() {
-	rng = rand.New(rand.NewPCG(123456789, 987654321))
+	once.Do(func() {
+		rng = rand.New(rand.NewPCG(123456789, 987654321))
+	})
 }
 
 // GenerateShortID - генерирует случайныый идентификатор заданной длины.
