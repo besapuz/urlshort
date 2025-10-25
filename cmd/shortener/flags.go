df9c2dcb6a2a57e0c35759481159fe46893c8979
Aleksandr <aleksandzarinskij18772@gmail.com>
2025-10-25 15:39:06 +0300

    iter5 - добавил регистрацию флага b

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -4,15 +4,16 @@ import (
 	"flag"
 )
 
-// неэкспортированная переменная flagRunAddr содержит адрес и порт для запуска сервера
-var flagRunAddr string
-
 // parseFlags обрабатывает аргументы командной строки
 // и сохраняет их значения в соответствующих переменных
 func parseFlags() {
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
+	var flagRunAddr string
 	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
+	var flagBaseURL string
+	flag.StringVar(&flagBaseURL, "b", "http://localhost:8080", "base URL for shortened links")
+
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
 }
