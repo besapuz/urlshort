11eeb1fda503dfaa775b12632af00c439db1b849
Aleksandr <aleksandzarinskij18772@gmail.com>
2025-11-09 21:15:02 +0300

    добавил зарытие бд при ошибке

--- internal/config/db/storage.go
+++ internal/config/db/storage.go
@@ -13,17 +13,21 @@ type DBStorage struct {
 
 // NewDBStorage - конструктор для DBStorage.
 func NewDBStorage(dsn string) (*DBStorage, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		return nil, fmt.Errorf("filed to database: %w", err)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
 
 	if err := db.PingContext(ctx); err != nil {
+		db.Close()
 		return nil, fmt.Errorf("filed to ping database: %w", err)
 	}
+
 	if err := createTable(db); err != nil {
+		db.Close()
 		return nil, fmt.Errorf("filed to create table: %w", err)
 	}
 	return &DBStorage{db: db}, nil
