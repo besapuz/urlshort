1242495a9cd10c2f0d9944e12c65215bc2cdc609
Aleksandr <aleksandzarinskij18772@gmail.com>
2025-11-09 23:01:43 +0300

    iter11 - изсправил создание таблицы, добаивил миграции.

--- internal/config/db/storage.go
+++ internal/config/db/storage.go
@@ -4,8 +4,13 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"path/filepath"
+	"runtime"
 	"time"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
@@ -27,6 +32,10 @@ func NewDBStorage(dsn string) (*DBStorage, error) {
 		db.Close()
 		return nil, fmt.Errorf("filed to ping database: %w", err)
 	}
+	if err := runMigrations(db); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("filed to run migrations: %w", err)
+	}
 
 	if err := createTable(db); err != nil {
 		db.Close()
@@ -35,27 +44,44 @@ func NewDBStorage(dsn string) (*DBStorage, error) {
 	return &DBStorage{db: db}, nil
 }
 
+// runMigrations - выполняет миграции БД.
+func runMigrations(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to create migration driver: %w", err)
+	}
+
+	// Получаем путь к миграциям относительно этого файла
+	_, filename, _, _ := runtime.Caller(0)
+	migrationsPath := filepath.Join(filepath.Dir(filename), "../../../migrations")
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://"+migrationsPath,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create migration instance: %w", err)
+	}
+
+	// Применяем миграции
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("failed to apply migrations: %w", err)
+	}
+
+	return nil
+}
+
 // createTable - функция для создания таблицы в базе данных.
 func createTable(db *sql.DB) error {
 	query := `
-	CREATE TABLE videos(
-	"video_id" TEXT,
-	"trending_date" TEXT,
-	"title" TEXT,
-	"channel_title" TEXT,
-	"category_id" INTEGER,
-	"publish_time" TEXT,
-	"tags" TEXT,
-	"views" INTEGER,
-	"likes" INTEGER,
-	"dislikes" INTEGER,
-	"comment_count" INTEGER,
-	"thumbnail_link" TEXT,
-	"comments_disabled" BOOLEAN,
-	"ratings_disabled" BOOLEAN,
-	"video_error_or_removed" BOOLEAN,
-	"description" TEXT
-	)`
+		CREATE TABLE IF NOT EXISTS url_mappings (
+		id SERIAL PRIMARY KEY,
+		uuid TEXT UNIQUE NOT NULL,
+		short_url TEXT UNIQUE NOT NULL,
+		original_url TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		)`
 	_, err := db.Exec(query)
 	return err
 }
